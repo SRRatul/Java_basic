74418282ccbdb98324d1de57c7e1f324f7bb3a0b
SRRatul <120864928+SRRatul@users.noreply.github.com>
2023-12-14 12:05:30 +0600

    Normal practice
    Here user will input his/her age and my system will print a message that your age is ... her/ his age.

--- Ratul.java
+++ Ratul.java
@@ -1,1 +1,10 @@
 import java.util.Scanner;
+public class Ratul{
+    public static void main(String[] args){
+        int i=0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        i = s.nextInt();
+        System.out.println("Your age is "+i);
+    }
+}
